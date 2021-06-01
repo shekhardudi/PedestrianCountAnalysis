db1874cf43f5c2445909f6539b456fcdfdc4e4f7
shekhardudi <shekhar.dudi@gmail.com>
2021-06-01 19:22:05 +1000

    Changed init

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # PedestrianCountAnalysis
-The application calculates -\
-1. Top 10 (most pedestrians) locations by day\
-2. Top 10 (most pedestrians) locations by month\
+The application calculates -  
+1. Top 10 (most pedestrians) locations by day
+2. Top 10 (most pedestrians) locations by month
 
-The source data is located in the data folder.\
+The source data is located in the data folder.
 
 # Appoach 
 The application performs an ETL operation as explained below - \
@@ -11,10 +11,53 @@ Step 1 - It takes the data from S3 bucket named - step-count-data-bucket-sh\
 Step 2 - It creates a data catalog by running a crawler - stepper-crawler-sh\
 Step 3 - It triggers an AWS glue Etl job with job name - count-steps-job-sh\
 Step 4 - It puts the transformed data in a csv format to another S3 bucket - step-count-output-bucket-sh\
-Step 5 - It puts the transformed data in postgres databases - rds-sh with datbase name MyDB\
+Step 5 - It puts the transformed data in postgres databases - rds-sh with datbase name MyDB
 
 # Architecture
 ![](img/Architecture.png)
 
 # Pre-Requisites
-AWS creadentials file
+- AWS Credentials
+- Terraform
+- python3.x
+
+# Data Files - 
+## !! This is important !!
+Download the Count file from  -   https://data.melbourne.vic.gov.au/Transport/Pedestrian-Counting-System-Monthly-counts-per-hour/b2ak-trbp
+
+Download the Sensor Data from  -   https://data.melbourne.vic.gov.au/Transport/Pedestrian-Counting-System-Sensor-Locations/h57g-5234
+
+Replace the files the data folder of the project.
+
+# Setup - 
+1. configure aws credentials - 
+    $ aws configure  
+
+    AWS Access Key ID [None]: your access key id XXXXX
+    AWS Secret Access Key [None]: your secret access key  XXXXX
+    Default region name [None]: ap-southeast-2
+    Default output format [None]: json
+
+2. Install terraform  - 
+    Follow - https://learn.hashicorp.com/tutorials/terraform/install-cli
+
+3. Create virtual environment in the project root folder(optional)
+    $ python3 -m venv .venv
+
+# Run - 
+1. Initiate terraform in the project root folder - 
+    $ terraform init
+
+2. Deploy the infra to aws  - 
+    $ terraform apply
+
+3. Activate virtual environment -
+    $ source .venv/bin/activate
+
+4. Install dependancies - 
+    $ pip install -r requirements.txt
+
+5.  Run the __init__.py script
+    $ python __init__.py 
+
+# Output
--- main.py
+++ main.py
@@ -1,123 +1,62 @@
-import pandas as pd
 import os
-import logging
-import boto3
-from botocore.exceptions import ClientError
+from Process.GluePorcessor import GlueProcessor
+from Process.S3Processor import S3Processor
 import config
-from botocore.exceptions import NoCredentialsError
-import time
-
-def upload(file_path, bucket):
-    file_dir, file_name = os.path.split(file_path)
-    try:
-        with open(file_path, "rb") as f:
-            s3.upload_fileobj(f,bucket,file_name)
-    except Exception as e:
-        print("Exception in uploading the file to S3")
-        print(e)
-        return False
-    return True
-
-def run_crawler(): 
-    try:
-        glue_client.start_crawler(Name=config.crawler_name)
-    except Exception as e:
-        print("Exception while running the Crwaler")
-        print(e)
-        return False
-    return True
 
 
-def run_job():
-    myNewJobRun = glue_client.start_job_run(JobName=config.glue_job_name)
-    return myNewJobRun
 
-def get_crawler_status():
-    count=0
-    tries = 100
-    response = False
-    while count<tries:
-        time.sleep(30)
-        crawler = glue_client.get_crawler(config.crawler_name)
-        crawler_status = crawler['LastCrawl']['Status']
-        print(crawler_status)
-        if "SUCCEEDED" == crawler_status:
-            response = True
-            break
+def upload_required_data():
+    print("Uploading Count File. This will take time kindly wait")
+    res = s3_obj.upload(file_path=os.path.join('data','Pedestrian_Counting_System_-_Monthly__counts_per_hour_.csv'),
+                    bucket=config.source_data_bucket)
+    if res:
+        print('Pedestrian Count Csv upload successful')
+        print("Uploading Sensor File")
+        res = s3_obj.upload(file_path=os.path.join('data','Pedestrian_Counting_System_-_Sensor_Locations.csv'),
+                bucket=config.source_data_bucket)
+        if res:
+            print('Sensor data uploaded successfully')
+        else:
+            print('Sensor upload failed. Can not continue')
+            exit(1)
+    else:
+        print('Pedestrian count upload failed. Can not continue')
+        exit(1)
 
-        elif "FAILED" == crawler_status or "CANCELLED" ==crawler_status:
-            break
+    res = s3_obj.upload(file_path='GlueJob.py',
+                bucket=config.script_bucket)
+    if res:
+        print('Glue Script uploaded successfully')
+    else:
+        print('Glue Script upload failed. Can not continue')
+        exit(1)
 
-        
-    return response
+def run_crawler():
+    res = glue_obj.run_crawler(crawler_name=config.crawler_name)
+    status = False
+    if res:
+        status = glue_obj.get_crawler_status(crawler_name=config.crawler_name)
+    if status:
+        print('Crawler Run Complete')
+    else:
+        print('Could not catalog data')
+        exit(1)  
 
-def get_job_status(job_run):
-    counter = 0
-    max_tries = 100
-    response = False
-    while counter < max_tries:
-        time.sleep(30)
-        status = glue_client.get_job_run(JobName=config.glue_job_name, RunId=job_run['JobRunId'])
-        print(status['JobRun']['JobRunState'])
-
-        if "SUCCEEDED" == status['JobRun']['JobRunState']:
-            response = True
-            break
-        elif "FAILED" == status['JobRun']['JobRunState'] or "CANCELLED" ==status['JobRun']['JobRunState']:
-            break
-
-        
-    return response
-
+def run_glue_job():
+    print('Starting Glue Job')
+    job_run = glue_obj.run_job(config.glue_job_name)
+    resp = glue_obj.get_job_status(job_name= config.glue_job_name,job_run=job_run)
+    if resp:
+        print("Glue Job finished Successfully!")
+    else:
+        print("Glue Job Failed")
 
 if __name__ == '__main__':
-    try:
-        # Create clients
-        s3 = boto3.client('s3')
-        glue_client = boto3.client('glue')
-        print("Process Started")
-        #Upload Csv Data
-        res = upload(file_path=os.path.join('data','Pedestrian_Counting_System_-_Monthly__counts_per_hour_.csv'),
-                    bucket=config.source_data_bucket)
-        if res:
-            print('Pedestrian Count Csv upload successful')
+    s3_obj = S3Processor()
+    glue_obj = GlueProcessor()
 
-            res = upload(file_path=os.path.join('data','Pedestrian_Counting_System_-_Sensor_Locations.csv'),
-                    bucket=config.source_data_bucket)
-            if res:
-                print('Sensor data uploaded successfully')
-            else:
-                print('Sensor upload failed. Can not continue')
-                exit(1)
-        else:
-            print('Pedestrian count upload failed. Can not continue')
-            exit(1)
-
-        res = upload(file_path='GlueJob.py',
-                    bucket=config.script_bucket)
-        if res:
-            print('Glue Script uploaded successfully')
-        else:
-            print('Glue Script upload failed. Can not continue')
-            exit(1)
-        
-        run_crawler()
-        status = get_crawler_status()
-
-        if status:
-            print('Crawler Run Complete')
-        else:
-            print('Could not catalog data')
-            exit(1)
-        
-        job_run = run_job()
-        resp = get_job_status(job_run=job_run)
-        if resp:
-            print("Glue Job finished Successfully!")
-        else:
-            print("Glue Job Failed")
-        
-
-    except Exception as e:
-        print(e)
-
+    print("Process Started")
+    
+    upload_required_data()
+    run_crawler()
+    run_glue_job()
